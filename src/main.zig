4674704b15c1afd7865f2cb2e744ddd0c330fa9a
koenigskraut <koenigskraut1@gmail.com>
2023-09-07 08:40:27 +0300

    delete main

--- src/main.zig
+++
@@ -1,7 +0,0 @@
-const std = @import("std");
-const testing = std.testing;
-
-comptime {
-    testing.refAllDeclsRecursive(@import("surface.zig"));
-    testing.refAllDeclsRecursive(@import("context.zig"));
-}
