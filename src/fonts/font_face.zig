9b66c889aae740c41c0bec85b74df5d7230bf3d5
koenigskraut <koenigskraut1@gmail.com>
2023-09-09 03:51:49 +0300

    abstract FontFace

--- src/fonts/font_face.zig
+++ src/fonts/font_face.zig
@@ -18,6 +18,98 @@ const Status = cairo.Status;
 const UserDataKey = cairo.UserDataKey;
 const DestroyFn = cairo.DestroyFn;
 
+pub fn Base(comptime Self: type) type {
+    return struct {
+        /// Increases the reference count on `self` by one. This prevents `self`
+        /// from being destroyed until a matching call to `.destroy()` is made.
+        ///
+        /// Use `cairo.FontFace.getReferenceCount()` to get the number of
+        /// references to a `cairo.FontFace`.
+        ///
+        /// **Returns**
+        ///
+        /// the referenced `cairo.FontFace`.
+        ///
+        /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-reference)
+        pub fn reference(self: *Self) *Self {
+            if (safety.tracing) safety.reference(@returnAddress(), self);
+            return @ptrCast(c.cairo_font_face_reference(self).?);
+        }
+
+        /// Decreases the reference count on `self` by one. If the result is zero,
+        /// then `self` and all associated resources are freed. See
+        /// `cairo.FontFace.reference()`.
+        ///
+        /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-destroy)
+        pub fn destroy(self: *Self) void {
+            c.cairo_font_face_destroy(self);
+            if (safety.tracing) safety.destroy(self);
+        }
+
+        /// Checks whether an error has previously occurred for this font face.
+        ///
+        /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-status)
+        pub fn status(self: *Self) Status {
+            return c.cairo_font_face_status(self);
+        }
+
+        /// This function returns the type of the backend used to create a font
+        /// face. See `cairo.FontFace.Type` for available types.
+        ///
+        /// **Returns**
+        ///
+        /// the type of `self`.
+        ///
+        /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-get-type)
+        pub fn getType(self: *Self) FontFace.Type {
+            return c.cairo_font_face_get_type(self);
+        }
+
+        /// Returns the current reference count of `self`.
+        ///
+        /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-get-reference-count)
+        pub fn getReferenceCount(self: *Self) usize {
+            return @intCast(c.cairo_font_face_get_reference_count(self));
+        }
+
+        /// Attach user data to `font_face`. To remove user data from a font face,
+        /// call this function with the key that was used to set it and `null` for
+        /// `data`.
+        ///
+        /// **Parameters**
+        /// - `key`: the address of a `cairo.UserDataKey` to attach the user data
+        /// to
+        /// - `user_data`: the user data to attach to the font face
+        /// - `destroyFn`: a `cairo.DestroyFn` which will be called when the font
+        /// face is destroyed or when new user data is attached using the same key.
+        ///
+        /// The only possible error is `error.OutOfMemory` if a slot could not be
+        /// allocated for the user data.
+        ///
+        /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-get-user-data)
+        pub fn setUserData(self: *Self, key: *const UserDataKey, user_data: ?*anyopaque, destroyFn: DestroyFn) CairoError!void {
+            try c.cairo_font_face_set_user_data(self, key, user_data, destroyFn).toErr();
+        }
+
+        /// Return user data previously attached to `self` using the specified key.
+        /// If no user data has been attached with the given key this function
+        /// returns `null`.
+        ///
+        /// **Parameters**
+        /// - `key`: the address of the `cairo.UserDataKey` the user data was
+        /// attached to
+        ///
+        /// **Returns**
+        ///
+        /// the user data previously attached or `null`.
+        ///
+        /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-set-user-data)
+        pub fn getUserData(self: *Self, key: *const UserDataKey) ?*anyopaque {
+            return c.cairo_font_face_get_user_data(self, key);
+        }
+    };
+}
+
 /// A `cairo.FontFace` specifies all aspects of a font other than the size or
 /// font matrix (a font matrix is used to distort a font by shearing it or
 /// scaling it unequally in the two directions). A font face can be set on a
@@ -32,6 +124,7 @@ const DestroyFn = cairo.DestroyFn;
 ///
 /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-t)
 pub const FontFace = opaque {
+    pub usingnamespace Base(@This());
     /// `cairo.FontFace.Type` is used to describe the type of a given font face
     /// or scaled font. The font types are also known as "font backends" within
     /// cairo.
@@ -92,92 +185,4 @@ pub const FontFace = opaque {
         /// Bold font weight
         Bold,
     };
-
-    /// Increases the reference count on `self` by one. This prevents `self`
-    /// from being destroyed until a matching call to `.destroy()` is made.
-    ///
-    /// Use `cairo.FontFace.getReferenceCount()` to get the number of
-    /// references to a `cairo.FontFace`.
-    ///
-    /// **Returns**
-    ///
-    /// the referenced `cairo.FontFace`.
-    ///
-    /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-reference)
-    pub fn reference(self: *FontFace) *FontFace {
-        if (safety.tracing) safety.reference(@returnAddress(), self);
-        return c.cairo_font_face_reference(self).?;
-    }
-
-    /// Decreases the reference count on `self` by one. If the result is zero,
-    /// then `self` and all associated resources are freed. See
-    /// `cairo.FontFace.reference()`.
-    ///
-    /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-destroy)
-    pub fn destroy(self: *FontFace) void {
-        c.cairo_font_face_destroy(self);
-        if (safety.tracing) safety.destroy(self);
-    }
-
-    /// Checks whether an error has previously occurred for this font face.
-    ///
-    /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-status)
-    pub fn status(self: *FontFace) Status {
-        return c.cairo_font_face_status(self);
-    }
-
-    /// This function returns the type of the backend used to create a font
-    /// face. See `cairo.FontFace.Type` for available types.
-    ///
-    /// **Returns**
-    ///
-    /// the type of `self`.
-    ///
-    /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-get-type)
-    pub fn getType(self: *FontFace) FontFace.Type {
-        return c.cairo_font_face_get_type(self);
-    }
-
-    /// Returns the current reference count of `self`.
-    ///
-    /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-get-reference-count)
-    pub fn getReferenceCount(self: *FontFace) usize {
-        return @intCast(c.cairo_font_face_get_reference_count(self));
-    }
-
-    /// Attach user data to `font_face`. To remove user data from a font face,
-    /// call this function with the key that was used to set it and `null` for
-    /// `data`.
-    ///
-    /// **Parameters**
-    /// - `key`: the address of a `cairo.UserDataKey` to attach the user data
-    /// to
-    /// - `user_data`: the user data to attach to the font face
-    /// - `destroyFn`: a `cairo.DestroyFn` which will be called when the font
-    /// face is destroyed or when new user data is attached using the same key.
-    ///
-    /// The only possible error is `error.OutOfMemory` if a slot could not be
-    /// allocated for the user data.
-    ///
-    /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-get-user-data)
-    pub fn setUserData(self: *FontFace, key: *const UserDataKey, user_data: ?*anyopaque, destroyFn: DestroyFn) CairoError!void {
-        try c.cairo_font_face_set_user_data(self, key, user_data, destroyFn).toErr();
-    }
-
-    /// Return user data previously attached to `self` using the specified key.
-    /// If no user data has been attached with the given key this function
-    /// returns `null`.
-    ///
-    /// **Parameters**
-    /// - `key`: the address of the `cairo.UserDataKey` the user data was
-    /// attached to
-    ///
-    /// **Returns**
-    ///
-    /// the user data previously attached or `null`.
-    ///
-    /// [Link to Cairo documentation](https://www.cairographics.org/manual/cairo-cairo-font-face-t.html#cairo-font-face-set-user-data)
-    pub fn getUserData(self: *FontFace, key: *const UserDataKey) ?*anyopaque {
-        return c.cairo_font_face_get_user_data(self, key);
-    }
 };
