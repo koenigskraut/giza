93c82d2362dc9b64425cdd681d72674e1a20bef2
koenigskraut <koenigskraut1@gmail.com>
2023-09-12 03:47:24 +0300

    pango quick fix

--- src/pango.zig
+++ src/pango.zig
@@ -1,5 +1,11 @@
-pub const safety = @import("safety.zig");
+const std = @import("std");
+pub const safety = @import("safety");
 
 pub const Context = @import("pango/context.zig").Context;
+pub const Layout = @import("pango/layout.zig").Layout;
 
 pub const c = @import("pango/c.zig");
+
+comptime {
+    std.testing.refAllDeclsRecursive(@This());
+}
