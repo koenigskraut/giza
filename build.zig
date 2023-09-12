1b7ffb8841cc8ccdae9dbda045bc76bd2778910b
koenigskraut <koenigskraut1@gmail.com>
2023-09-12 03:41:05 +0300

    clean up build.zig

--- build.zig
+++ build.zig
@@ -4,55 +4,40 @@ const std = @import("std");
 // declaratively construct a build graph that will be executed by an external
 // runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
+    _ = target;
     const optimize = b.standardOptimizeOption(.{});
+    _ = optimize;
 
-    const lib = b.addStaticLibrary(.{
-        .name = "giza",
-        // In this case the main source file is merely a path, however, in more
-        // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/main.zig" },
-        .target = target,
-        .optimize = optimize,
-    });
-
-    // This declares intent for the library to be installed into the standard
-    // location when the user invokes the "install" step (the default step when
-    // running `zig build`).
-    b.installArtifact(lib);
+    const opts = b.addOptions();
+    opts.addOption(bool, "no-pango", b.option(bool, "no-pango", "disable pango support") orelse false);
 
     var safety_module = b.addModule("safety", .{ .source_file = .{ .path = "src/safety.zig" } });
 
-    var module = b.addModule("cairo", .{
+    var cairo_module = b.addModule("cairo", .{
         .source_file = .{ .path = "src/cairo.zig" },
         .dependencies = &.{
+            .{ .name = "build_options", .module = opts.createModule() },
             .{ .name = "safety", .module = safety_module },
         },
     });
-    // Creates a step for unit testing. This only builds the test executable
-    // but does not run it.
-    const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/cairo.zig" },
-        .target = target,
-        .optimize = optimize,
+    var pango_module = b.addModule("pango", .{
+        .source_file = .{ .path = "src/pango.zig" },
+        .dependencies = &.{
+            .{ .name = "build_options", .module = opts.createModule() },
+            .{ .name = "cairo", .module = cairo_module },
+            .{ .name = "safety", .module = safety_module },
+        },
     });
-    main_tests.addModule("cairo", module);
-    main_tests.linkLibC();
-    main_tests.linkSystemLibrary("cairo");
-
-    const run_main_tests = b.addRunArtifact(main_tests);
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build test`
-    // This will evaluate the `test` step rather than the default, which is "install".
-    const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&run_main_tests.step);
+    _ = pango_module;
+    // var pangocairo_module = b.addModule("pangocairo", .{
+    //     .source_file = .{ .path = "src/pangocairo.zig" },
+    //     .dependencies = &.{
+    //         .{ .name = "build_options", .module = opts.createModule() },
+    //         .{ .name = "cairo", .module = cairo_module },
+    //         .{ .name = "pango", .module = pango_module },
+    //         .{ .name = "safety", .module = safety_module },
+    //     },
+    // });
+    // cairo_module.dependencies.put("pangocairo", pangocairo_module) catch @panic("OOM");
 }
