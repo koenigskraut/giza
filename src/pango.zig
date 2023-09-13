7b4cee42e05dd909ca4333038e9cce136581bee2
koenigskraut <koenigskraut1@gmail.com>
2023-09-13 07:07:04 +0300

    more pango.Context functions

--- src/pango.zig
+++ src/pango.zig
@@ -8,6 +8,9 @@ pub usingnamespace @import("pango/util.zig");
 pub const Context = @import("pango/context.zig").Context;
 pub const Layout = @import("pango/layout.zig").Layout;
 pub const Language = @import("pango/language.zig").Language;
+pub const FontDescription = @import("pango/font_description.zig").FontDescription;
+pub const FontMap = @import("pango/font_map.zig").FontMap;
+pub const FontFamily = @import("pango/font_family.zig").FontFamily;
 
 pub const c = @import("pango/c.zig");
 
---
+++ src/pango/font_family.zig
@@ -0,0 +1,1 @@
+pub const FontFamily = opaque {};
