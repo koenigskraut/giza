2e576a64950d08a293271e1c2bf9518d221ab5dc
koenigskraut <koenigskraut1@gmail.com>
2023-09-13 07:06:00 +0300

    more fixes

--- src/pango/util.zig
+++ src/pango/util.zig
@@ -3,7 +3,7 @@
 /// `pango.Rectangle` is frequently used to represent the logical or ink
 /// extents of a single glyph or section of text. (See, for instance,
 /// `pango.Font.getGlyphExtents()`.)
-pub const Rectangle = struct {
+pub const Rectangle = extern struct {
     /// X coordinate of the left side of the rectangle.
     x: c_int,
     /// Y coordinate of the the top side of the rectangle.
